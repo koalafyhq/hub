6d3f3d7307d6823892e06aa2a7d74fcef5883b2e
fariz <fariz@koalafyhq.com>
2020-12-31 17:54:50 +0800

    feat: create initial _app.js

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import { Provider } from 'next-auth/client'
+import { Box, ChakraProvider } from '@chakra-ui/react'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider session={pageProps.session}>
+      <ChakraProvider>
+        <Box bg='blue.900' height='100vh'>
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </Provider>
+  )
+}
