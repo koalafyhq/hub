857d63db2ee3ae89635fddcbb5095659caee7a8e
fariz <fariz@koalafyhq.com>
2020-12-31 19:10:28 +0800

    feat: create constants

---
+++ utils/constants.js
@@ -0,0 +1,18 @@
+export const DEPLOYMENT_STATUS = {
+  NOT_READY: 'NOT_READY',
+  READY: 'READY',
+  BUILDING: 'BUILDING',
+  CANCELLED: 'CANCELLED',
+  FAILED: 'FAILED',
+}
+
+export const BUILDER_TYPE = {
+  nodejs: 'Node.js',
+  hugo: 'Hugo',
+}
+
+export const PROJECT_TYPE = {
+  STATIC: 'Statis',
+  WORDPRESS: 'Wordpress',
+  GHOST: 'Ghost',
+}
